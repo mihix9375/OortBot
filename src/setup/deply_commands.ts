a0841e743a69c6474a528fad75676151a126c8ce
mihix9375 <mihix5.100@gmail.com>
2026-08-21 19:54:12 +0900

    Fix: Correct relative paths in deply_commands.ts after restructuring

--- src/setup/deply_commands.ts
+++ src/setup/deply_commands.ts
@@ -1,13 +1,13 @@
 const { REST, Routes } 		= require("discord.js");
-const { clientId, token } 	= require("./../config.json");
+const { clientId, token } 	= require("../../config.json");
 const fs 					= require("node:fs");
 const rest 					= new REST({ version: "10" }).setToken(token);
 const args 					= process.argv.slice(2);
 const commands 				= [];
-const commandFiles 			= fs.readdirSync("./commands").filter(file => file.endsWith(""));
+const commandFiles 			= fs.readdirSync("./commands").filter(file => file !== "src");
 
 for (const file of commandFiles) {
-	const command = require(`./../commands/${file}`);
+	const command = require(`../../commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
